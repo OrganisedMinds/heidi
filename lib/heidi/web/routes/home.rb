de206e9b38ff1a73ddecd4033d01f9cdbe7977e4
Hartog C. de Mik <hartog@organisedminds.com>
2012-02-01 16:48:45 +0100

    Class based sinatra plze

--- lib/heidi/web/routes/home.rb
+++
@@ -1,9 +0,0 @@
-class Heidi; module Web; module Routes
-
-  module Home
-    get '/' do
-      '<h1>Welcome to Heidi!</h1>Psst. Want to see my <a href="/projects">projects</a>?'
-    end
-  end
-
-end; end; end
