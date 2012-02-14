31c3fd135aff1c4ff41920ceb51780ab3c15c885
Hartog C. de Mik <hartog.de.mik@gmail.com>
2012-02-14 22:46:40 +0100

    Configure heidi through Heidi::Web

--- lib/heidi/web.rb
+++ lib/heidi/web.rb
@@ -109,6 +109,20 @@ class Heidi
       erb(:config, { :locals => { :project => project } } )
     end
 
+    post '/projects/:project_name' do
+      # update project
+      project = @heidi[params[:project_name]]
+      if project.nil?
+        return "no project by that name: #{params[:project_name]}"
+      end
+
+      project.name = params[:name]
+      project.integration_branch = params[:branch]
+
+
+      redirect "/projects/#{project.basename}", 302
+    end
+
     helpers do
       def ansi_color_codes(string)
         return "" if string.nil?
