73eb76ff5a5ee22e2c8b72f91cf8813352973872
Hartog C. de Mik <hartog.de.mik@gmail.com>
2012-02-14 21:42:20 +0100

    Link commits in the commit log

--- lib/heidi/web.rb
+++ lib/heidi/web.rb
@@ -112,6 +112,8 @@ class Heidi
 
     helpers do
       def ansi_color_codes(string)
+        return "" if string.nil?
+
         string.gsub(/\e\[0?m/, '</span>').
           gsub /\e\[[^m]+m/ do |codes|
             colors = codes.gsub(/[\e\[m]+/, '')
