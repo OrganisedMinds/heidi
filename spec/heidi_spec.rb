7f4535116eb30e6cf23ecb1e51ab2bcbb2f8de00
Hartog C. de Mik <hartog.de.mik@gmail.com>
2012-02-21 17:31:05 +0100

    Remove fake heidi when done

--- spec/heidi_spec.rb
+++ spec/heidi_spec.rb
@@ -5,6 +5,10 @@ describe Heidi do
     fake_me_a_heidi
   end
 
+  after(:all) do
+    FileUtils.remove_entry_secure @fake
+  end
+
   it "should gather projects on load" do
     @heidi.projects.should be_kind_of(Array)
     @heidi.projects.count.should == 1
