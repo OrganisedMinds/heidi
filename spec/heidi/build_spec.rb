da6a290c6948e870e585f82ed5621ede74c7d20a
Hartog C. de Mik <hartog.de.mik@gmail.com>
2012-02-22 09:26:28 +0100

    First tests defined

--- spec/heidi/build_spec.rb
+++ spec/heidi/build_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
 describe Heidi::Build do
-  it "has no tests" do
+  it "should have a project" do
+    pending "test please"
+  end
+
+  it "should be tied to a commit" do
+    pending "test please"
+  end
+  it "has not enough tests" do
     pending "more tests please!"
   end
 end
