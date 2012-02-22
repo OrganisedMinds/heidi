e66f72b0a75479e56f5e68d4434a798d5b619166
Hartog C. de Mik <hartog@organisedminds.com>
2012-02-22 17:06:08 +0100

    More specs defined

--- spec/heidi/build_spec.rb
+++ spec/heidi/build_spec.rb
@@ -1,13 +1,58 @@
 require 'spec_helper'
 
 describe Heidi::Build do
+  before(:all) do
+    fake_me_a_heidi
+
+    @project = @heidi[:heidi_test]
+  end
+
+  after(:all) do
+    FileUtils.remove_entry_secure @fake
+  end
+
+  before :each do
+    @build = Heidi::Build.new(@project)
+  end
+
   it "should have a project" do
-    pending "test please"
+    @build.project.should == @project
   end
 
   it "should be tied to a commit" do
-    pending "test please"
+    @build.commit.should == @project.commit
   end
+
+  it "should have a root" do
+    @build.root.should == File.join(@project.root, "logs", @project.commit)
+  end
+
+  it "should have a log root" do
+    @build.log_root.should == File.join(@build.root, "logs")
+  end
+
+  it "should have a build root" do
+    @build.build_root.should == File.join(@build.root, "build")
+  end
+
+  describe "Locking" do
+    it "should not be locked" do
+      @build.should_not be_locked
+    end
+
+    it "should be lockable"
+    it "should be block-lockable"
+    it "should be unlockable"
+  end
+
+  describe "Logging" do
+    it "should have logs"
+    it "should write to a log file"
+    it "should read from a log file"
+    it "should have convenience methods for heidi.* logs"
+    it "should have x"
+  end
+
   it "has not enough tests" do
     pending "more tests please!"
   end
