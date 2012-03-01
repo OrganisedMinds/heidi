780ad1e10d65ad6b993dfed919a66a02d67fcd03
Hartog C. de Mik <hartog.de.mik@gmail.com>
2012-03-01 14:49:28 +0100

    Finalized web specs

--- spec/heidi/web_spec.rb
+++ spec/heidi/web_spec.rb
@@ -9,6 +9,8 @@ describe Heidi::Web do
 
   before :each do
     Heidi::Web.stub(:project_path).and_return(@fake)
+    @project = @heidi[:heidi_test]
+    @project.name = "Heidi Test Project on the web"
   end
 
   after :all do
@@ -34,11 +36,44 @@ describe Heidi::Web do
 
   it "should display the projects on the homepage" do
     get '/projects/'
+    last_response.status.should == 200
     last_response.body.should =~ /heidi_test/
   end
 
-  it "should display the project page"
-  it "should display the configuration page"
-  it "should display a build"
-  it "should display a commit"
+  it "should display the project page" do
+    get '/projects/heidi_test'
+    last_response.status.should == 200
+    last_response.body.should =~ /<h1>Heidi Test Project on the web<\/h1>/
+  end
+
+  it "should display the configuration page" do
+    get '/projects/heidi_test/configure'
+    last_response.status.should == 200
+    last_response.body.should =~ /Configure Heidi Test Project on the web/
+  end
+
+  describe "Project internals" do
+    before :each do
+      @project.integrate
+    end
+
+    it "should display a build" do
+      get "/projects/heidi_test/build/#{@project.builds.last.commit}"
+      last_response.status.should == 200
+
+      title = Regexp.new("<h2 class=[^>]+>#{@project.builds.last.commit}</h2>")
+      last_response.body.should =~ title
+    end
+
+    it "should display a commit" do
+      get "/projects/heidi_test/commit/#{@project.commit}"
+      last_response.status.should == 200
+      last_response.body.should =~ Regexp.new("<h2>#{@project.commit}</h2>")
+
+      author = @project.author.gsub("<", "&lt;")
+      author.gsub!(">", "&gt;")
+      check = Regexp.new("Author: #{author}")
+      last_response.body.should =~ check
+    end
+  end
 end
